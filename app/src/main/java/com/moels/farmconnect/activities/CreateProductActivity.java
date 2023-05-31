04415595f2347300faffd781132bb509c2ccea07
Muhereza Joel <muherezajoel40@gmail.com>
2023-05-31 21:13:01 +0300

    Added crop funtionality in create product activity

--- app/src/main/java/com/moels/farmconnect/activities/CreateProductActivity.java
+++ app/src/main/java/com/moels/farmconnect/activities/CreateProductActivity.java
@@ -1,20 +1,31 @@
 package com.moels.farmconnect.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.moels.farmconnect.R;
 import com.moels.farmconnect.utility_classes.UI;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class CreateProductActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 1;
     private Toolbar toolbar;
+    private ImageView productImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +36,40 @@ public class CreateProductActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         UI.setUpActionBar(getSupportActionBar(),R.drawable.ic_back_arrow, "Create New Product", true);
+
+        productImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, PICK_IMAGE_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            Uri uri = data.getData();
+            CropImage.activity(uri).setActivityTitle("Crop Image")
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(this);
+
+        } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            Uri croppedImageURI = result.getUri();
+
+            Glide.with(this)
+                    .load(croppedImageURI)
+                    .into(productImageView);
+
+        }
     }
 
     private void initUI(){
         toolbar = findViewById(R.id.create_product_activity_toolbar);
+        productImageView = findViewById(R.id.product_image_view);
     }
 
     private void setUpStatusBar(){
